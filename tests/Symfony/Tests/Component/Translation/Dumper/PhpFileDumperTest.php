31ef81e9f401132db636ddd5c2cf55f6084aa3c7
Michel Salib <michelsalib@hotmail.com>
2011-08-28 19:12:35 +0200

    -- add dumpers for translation component
    -- update license and phpDoc
    
    -- fix CS
    
    -- remove pot file loader
    
    -- add unit tests
    
    -- update unit tests

---
+++ tests/Symfony/Tests/Component/Translation/Dumper/PhpFileDumperTest.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Tests\Component\Translation\Dumper;
+
+use Symfony\Component\Translation\MessageCatalogue;
+use Symfony\Component\Translation\Dumper\PhpDumper;
+
+class PhpFileDumperTest extends \PHPUnit_Framework_TestCase
+{
+    public function testDump()
+    {
+        $catalogue = new MessageCatalogue('en');
+        $catalogue->add(array('foo' => 'bar'));
+        
+        $dumper = new PhpDumper();
+        $dumperString = $dumper->dump($catalogue);
+        
+        $resource = __DIR__.'/../fixtures/resources.php';
+        $fileString = file_get_contents($resource);
+        
+        $this->assertEquals($fileString, $dumperString);
+    }
+}
